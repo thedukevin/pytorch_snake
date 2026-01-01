ea223e5053020eb4309744219f211eab18a1be5c
Kevin Du <kevdu3435@gmail.com>
2025-12-31 23:28:40 -0500

    Added GAE and controller logic

--- snake.py
+++ snake.py
@@ -20,8 +20,16 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device " + str(device))
 
 boardSize = 10
-maxTime = 100
+maxTime = 20
 discountRate = 0.99
+GAEparam = 0.95
+
+# Network params
+value_coef = 0.5
+entropy_coef = 0.01
+learning_rate = 1e-03
+
+
 
 dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
@@ -35,63 +43,6 @@ appleReward = 1
 winReward = 0
 loseReward = 0
 
-# Network params
-value_coef = 0.5
-entropy_coef = 0.01
-
-# class Environment:
-#     def __init__(self):
-#         self.head = (boardSize // 2, boardSize // 2)
-#         self.randomizeApple()
-#         self.time = 0
-    
-#     def makeAction(self, action):
-#         newHead = shiftPos(self.head, action)
-#         # while not isValid(newHead):
-#         #     action = np.random.randint(4)
-#         #     newHead = shiftPos(self.head, action)
-#         if not isValid(newHead):
-#             return loseReward, True
-#         self.head = newHead
-
-#         reward = 0
-#         if self.head == self.apple:
-#             self.randomizeApple()
-#             reward = appleReward
-        
-#         self.time += 1
-#         return reward, self.time == maxTime
-    
-#     def randomizeApple(self):
-#         openSquares = [(i, j) for i in range(boardSize) for j in range(boardSize) if (i, j) != self.head]
-#         self.apple = openSquares[np.random.randint(len(openSquares))]
-    
-#     def toTensor(self):
-#         arr = np.zeros((2, boardSize, boardSize))
-#         arr[0, self.head[0], self.head[1]] = 1
-#         arr[1, self.apple[0], self.apple[1]] = 1
-#         return torch.tensor(arr).to(torch.float32)
-    
-#     def bestActions(self):
-#         dists = []
-#         for d in range(4):
-#             newHead = shiftPos(self.head, d)
-#             dists.append(abs(newHead[0] - self.apple[0]) + abs(newHead[1] - self.apple[1]))
-#         return [i for i in range(4) if dists[i] == min(dists)]
-
-#     def __str__(self):
-#         s = "Time: " + str(self.time) + '\n'
-#         for i in range(boardSize):
-#             for j in range(boardSize):
-#                 if (i, j) == self.head:
-#                     s += 'H'
-#                 elif (i, j) == self.apple:
-#                     s += 'A'
-#                 else:
-#                     s += '.'
-#             s += '\n'
-#         return s
-
 class Environment:
     def __init__(self):
         self.body = np.full((boardSize, boardSize), -1)
@@ -101,38 +52,56 @@ class Environment:
         self.randomizeApple()
         self.time = 0
     
-    def validActions(self):
-        acts = []
+    def validDirs(self):
+        validDirs = []
         for d in range(4):
             newHead = shiftPos(self.head, d)
-            acts.append(int(isValid(newHead) and self.queryBody(newHead) == -1))
-        return torch.tensor(acts)
+            if isValid(newHead) and self.queryBody(newHead) == -1:
+                validDirs.append(d)
+        return validDirs
 
     def makeAction(self, action):
-        assert self.validActions()[action]
-        newHead = shiftPos(self.head, action)
+        target = (action // boardSize, action % boardSize)
 
-        if not isValid(newHead) or self.queryBody(newHead) != -1:
-            return loseReward, True
-        self.editBody(self.head, action)
-        self.head = newHead
+        reward = -0.01
+        init_time = self.time
+        while target != self.head:
+            dirs = self.validDirs()
+            if len(dirs) == 0:
+                print(self)
+            assert len(dirs) > 0
+            minDist = 1e+10
+            action = -1
+            for d in dirs:
+                newHead = shiftPos(self.head, d)
+                newDist = abs(newHead[0] - target[0]) + abs(newHead[1] - target[1])
+                if newDist < minDist:
+                    minDist = newDist
+                    action = d
+            assert action >= 0
+            self.editBody(self.head, action)
+            self.head = shiftPos(self.head, action)
 
-        reward = 0
-        if self.head == self.apple:
-            if np.sum(self.body) == 1:
-                return winReward, True
-            self.randomizeApple()
-            reward = appleReward
-        else:
-            tailDir = self.queryBody(self.tail)
-            self.editBody(self.tail, -1)
-            self.tail = shiftPos(self.tail, tailDir)
-        
-        if (self.validActions() == 0).all():
-            return loseReward, True
-        
-        self.time += 1
-        return reward, self.time == maxTime or np.sum(self.body == -1) == 1
+            self.time += 1
+
+            if self.head == self.apple:
+                if np.sum(self.body == -1) == 1:
+                    return winReward, True
+                if len(self.validDirs()) == 0:
+                    return loseReward, True
+                self.randomizeApple()
+                reward = appleReward * discountRate ** (self.time - init_time)
+                break
+            else:
+                tailDir = self.queryBody(self.tail)
+                self.editBody(self.tail, -1)
+                self.tail = shiftPos(self.tail, tailDir)
+            
+            if len(self.validDirs()) == 0:
+                return loseReward, True
+            
+            
+        return reward, False
     
     def editBody(self, pos, newElement):
         self.body[pos[0]][pos[1]] = newElement
@@ -170,27 +139,25 @@ class Environment:
 
 # env = Environment()
 # print(env)
-# print(env.makeAction(0))
+# print(env.makeAction(53))
 # print(env)
-# print(env.makeAction(1))
-# print(env.makeAction(1))
-# print(env.makeAction(1))
-# print(env.makeAction(1))
-# print(env.makeAction(0))
-# print(env.makeAction(0))
+# print(env.makeAction(81))
 # print(env)
-# print(env.makeAction(2))
-# print(env.toTensor())
+# print(env.makeAction(95))
+# print(env)
+# print(env.makeAction(14))
+# print(env)
+
 
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(7, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 16, 3, padding=1)
+        self.conv1 = nn.Conv2d(7, 64, 3, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 5 * 5, 128)
-        self.policy = nn.Linear(128, 4)
-        self.value = nn.Linear(128, 1)
+        self.fc1 = nn.Linear(64 * 5 * 5, 512)
+        self.policy = nn.Linear(512, 100)
+        self.value = nn.Linear(512, 1)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -206,7 +173,7 @@ class PolicyGradient():
 
         self.model = CNN().to(device)
         
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
 
         self.gameOutput = "game.txt"
         with open(self.gameOutput, 'w') as f:
@@ -227,7 +194,9 @@ class PolicyGradient():
             trajectories.append([{
                 'env': Environment(),
                 'reward': None,
-                'value': None
+                'emp_val': None,
+                'evaluation': None,
+                'GAE': None
             }])
         value_hist = []
         action_hist = []
@@ -238,41 +207,56 @@ class PolicyGradient():
             for i in active_thread_hist[t]:
                 # if i == 0:
                 #     print(trajectories[i][t]['env'])
+                # print(trajectories[i][t]['env'])
                 inputs.append(trajectories[i][t]['env'].toTensor())
             logits, value = self.model(torch.stack(inputs).to(device))
 
-            for index, i in enumerate(active_thread_hist[t]):
-                va = trajectories[i][t]['env'].validActions().to(device)
-                logits[index, :] = logits[index, :].masked_fill(va == 0, float('-inf'))
+            # for index, i in enumerate(active_thread_hist[t]):
+            #     va = trajectories[i][t]['env'].validActions().to(device)
+            #     logits[index, :] = logits[index, :].masked_fill(va == 0, float('-inf'))
             
             dist = Categorical(logits=logits)
+
             actions = dist.sample()
             log_prob = dist.log_prob(actions)
             new_active_threads = copy.deepcopy(active_thread_hist[t])
             for index, i in enumerate(active_thread_hist[t]):
                 new_env = copy.deepcopy(trajectories[i][t]['env'])
+                trajectories[i][t]['evaluation'] = value[index].item()
                 trajectories[i][t]['reward'], endState = new_env.makeAction(actions[index])
+                # print("Action: " + str(actions[index]))
+                # print("Endstate: " + str(endState))
                 if endState:
                     new_active_threads.remove(i)
-                else:
-                    trajectories[i].append({
-                        'env': new_env,
-                        'reward': None,
-                        'value': None
-                    })
+                # elif t < maxTime-1:
+                trajectories[i].append({
+                    'env': new_env,
+                    'reward': 0,
+                    'emp_val': None,
+                    'evaluation': 0,
+                    'GAE': None
+                })
             value_hist.append(value.reshape((-1,)))
             action_hist.append(actions)
             log_prob_hist.append(log_prob)
             dist_hist.append(dist)
             if len(new_active_threads) == 0:
                 break
-            active_thread_hist.append(new_active_threads)
+            if t < maxTime-1:
+                active_thread_hist.append(new_active_threads)
+        # print(trajectories)
         for i in range(n_threads):
             val = 0
-            for t in range(len(trajectories[i])-1, -1, -1):
-                val *= discountRate
-                val += trajectories[i][t]['reward']
-                trajectories[i][t]['value'] = val
+            emp_val = 0
+            for t in range(len(trajectories[i])-2, -1, -1):
+                time_diff = trajectories[i][t+1]['env'].time - trajectories[i][t]['env'].time
+                val = (trajectories[i][t]['reward'] 
+                       + discountRate**time_diff * trajectories[i][t+1]['evaluation'] 
+                       - trajectories[i][t]['evaluation']
+                       + (discountRate * GAEparam) ** time_diff * val)
+                trajectories[i][t]['GAE'] = val
+                emp_val = trajectories[i][t]['reward'] + discountRate**time_diff * emp_val
+                trajectories[i][t]['emp_val'] = emp_val
         return trajectories, active_thread_hist, value_hist, action_hist, log_prob_hist, dist_hist
 
     def updateModel(self, trajectories, active_thread_hist, value_hist, action_hist, log_prob_hist, dist_hist):
@@ -281,14 +265,19 @@ class PolicyGradient():
         for t in range(len(active_thread_hist)):
             inputs = []
             emp_vals = []
+            advantages = []
             for i in active_thread_hist[t]:
                 inputs.append(trajectories[i][t]['env'].toTensor())
-                emp_vals.append(trajectories[i][t]['value'])
+                emp_vals.append(trajectories[i][t]['emp_val'])
+                advantages.append(trajectories[i][t]['GAE'])
             logits, value = self.model(torch.stack(inputs).to(device))
             emp_vals = torch.tensor(emp_vals).to(device)
-            advantage = (emp_vals - value.reshape((-1,))).detach()
+            advantages = torch.tensor(advantages).to(device)
+            # advantage = (emp_vals - value.reshape((-1,))).detach()
 
-            loss = (-log_prob_hist[t] * advantage + value_coef * torch.pow(value_hist[t] - emp_vals, 2) - entropy_coef * dist_hist[t].entropy()).mean()
+            loss = (-log_prob_hist[t] * advantages 
+                    + value_coef * (value_hist[t] - emp_vals) ** 2 
+                    - entropy_coef * dist_hist[t].entropy()).mean()
 
             loss.backward()
         self.optimizer.step()
@@ -296,8 +285,12 @@ class PolicyGradient():
     def trainLoop(self, n_iter, n_threads, evalPeriod, include_accuracy=False):
 
         scoreHist = []
+        sizeHist = []
+        lengthsHist = []
         timesHist = []
         accuracyHist = []
+        winHist = []
+        lossHist = []
 
         with open(self.mainOutput, 'a') as f:
             f.write("Starting training, Previous iterations: " + str(self.itCount) + " Current iterations: " + str(n_iter) + " Timestamp: " + str(datetime.now()) + '\n')
@@ -309,16 +302,29 @@ class PolicyGradient():
             # Evaluation
 
             scores = []
+            sizes = []
+            lengths = []
             times = []
+            wins = []
+            losses = []
 
             for traj in trajectories:
                 # size = (traj[-1]['env'].body != -1).sum() + 1
                 # sizes.append(size)
-                score = sum(s['reward'] for s in traj)
+                score = sum(s['reward'] for s in traj[:-1])
                 scores.append(score)
 
-                survive_time = len(traj)
-                times.append(survive_time)
+                size = (traj[-1]['env'].body != -1).sum() + 1
+                sizes.append(size)
+
+                game_length = len(traj)-1
+                lengths.append(game_length)
+
+                elapsed_time = traj[-1]['env'].time
+                times.append(elapsed_time)
+
+                wins.append((traj[-1]['env'].body == -1).sum() == 1)
+                losses.append(len(traj[-1]['env'].validDirs()) == 0)
             
             if include_accuracy:
                 acc = []
@@ -333,19 +339,27 @@ class PolicyGradient():
                 accuracyHist.append(np.array(acc).mean())
             
             scoreHist.append(np.array(scores).mean())
+            sizeHist.append(np.array(sizes).mean())
             timesHist.append(np.array(times).mean())
+            lengthsHist.append(np.array(lengths).mean())
+            winHist.append(np.array(wins).mean())
+            lossHist.append(np.array(losses).mean())
 
             self.itCount += 1
             if self.itCount % evalPeriod == 0:
                 with open(self.mainOutput, 'a') as f:
                     f.write("Iteration: " + str(self.itCount) + 
                         " Score: " + str(np.array(scoreHist[-evalPeriod:]).mean()) +
-                        " Game Length: " + str(np.array(timesHist[-evalPeriod:]).mean()) + 
+                        " Size: " + str(np.array(sizeHist[-evalPeriod:]).mean()) +
+                        " Game Length: " + str(np.array(lengthsHist[-evalPeriod:]).mean()) + 
+                        " Elapsed Time: " + str(np.array(timesHist[-evalPeriod:]).mean()) + 
+                        " Wins: " + str(np.array(winHist[-evalPeriod:]).mean()) + 
+                        " Losses: " + str(np.array(lossHist[-evalPeriod:]).mean()) + 
                         (" Accuracy: " + str(np.array(accuracyHist[-evalPeriod:]).mean()) if include_accuracy else "") + 
                         " Timestamp: " + str(datetime.now()) + '\n')
 
                 with open(self.gameOutput, 'a') as f:
-                    s = "----------------------------- Iteration " + str(it) + ' -----------------------------\n'
+                    s = "----------------------------- Iteration " + str(self.itCount) + ' -----------------------------\n'
                     for index in range(n_threads):
                         s += '"################ Thread ' + str(index) + ' ################\n'
                         for t in range(len(active_thread_hist)):
@@ -355,7 +369,7 @@ class PolicyGradient():
                                 continue
                             s += str(trajectories[index][t]['env']) + '\n'
                             s += 'Reward: ' + str(trajectories[index][t]['reward']) + '\n'
-                            s += 'Value: ' + str(trajectories[index][t]['value']) + '\n'
+                            s += 'Emp Val: ' + str(trajectories[index][t]['emp_val']) + '\n'
                             s += 'Action: ' + str(action_hist[t][i]) + '\n'
                             s += 'Probs: ' + str(dist_hist[t].probs[i]) + '\n'
                             s += 'Net value: ' + str(value_hist[t][i]) + '\n'
@@ -365,33 +379,6 @@ class PolicyGradient():
                 with open('snake.pkl', 'wb') as file:
                     pickle.dump(self, file)
             
-            # if it == 3000:
-            #     import os, shutil
-            #     folder = 'snake_traj'
-            #     for filename in os.listdir(folder):
-            #         file_path = os.path.join(folder, filename)
-            #         try:
-            #             if os.path.isfile(file_path) or os.path.islink(file_path):
-            #                 os.unlink(file_path)
-            #             elif os.path.isdir(file_path):
-            #                 shutil.rmtree(file_path)
-            #         except Exception as e:
-            #             print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-            #     for t in range(len(active_thread_hist)):
-            #         for index, i in enumerate(active_thread_hist[t]):
-            #             s = trajectories[i][t]['env']
-            #             plt.figure()
-            #             snake = (s.body != -1) * 0.5
-            #             snake[s.head[0]][s.head[1]] = 0.5
-            #             snake[s.apple[0]][s.apple[1]] = 1
-            #             plt.imshow(snake)
-            #             plt.title(f"Reward: {trajectories[i][t]['reward']} Value: {trajectories[i][t]['value']} Action: {action_hist[t][index]}")
-            #             plt.xlabel(f"Probs: {prob_hist[t][index]}")
-            #             plt.savefig(f"snake_traj/state{i}_{t}")
-            #             plt.close()
-
-    # trainLoop(1, 4, 10)
 
 mode = "WRITE"
 
